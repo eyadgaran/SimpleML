41b8bc4f86caf071d374f2773822916fc4ce621e
Elisha Yadgaran <ElishaY@alum.mit.edu>
2018-01-20 21:30:39 -0500

    initial package setup and pypi upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpleml',
+    version='0.1',
+    description='Simplified Machine Learning',
+    author='Elisha Yadgaran',
+    author_email='ElishaY@alum.mit.edu',
+    license='BSD-3',
+    url='https://github.com/eyadgaran/SimpleML',
+    download_url='https://github.com/eyadgaran/SimpleML/archive/0.1.tar.gz',
+    packages=find_packages(),
+    keywords = ['machine learning'],
+    install_requires=[],
+    zip_safe=False
+)
