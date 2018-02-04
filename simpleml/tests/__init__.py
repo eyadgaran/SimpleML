a82f896640f67f6dfa8804f4bef4c409fa0edb1d
Elisha Yadgaran <ElishaY@alum.mit.edu>
2018-02-04 16:44:29 -0500

    initial testing structure

--- simpleml/tests/__init__.py
+++ simpleml/tests/__init__.py
@@ -1,1 +1,25 @@
+'''
+Setup testing env
+'''
+
+from simpleml.utils.initialization import Database
+from subprocess import call
+import random
+
 __author__ = 'Elisha Yadgaran'
+
+TEST_DATABASE = 'SimpleML-TEST-{}'.format(random.randint(10000, 99999))
+
+
+def setup_package():
+    print 'Setting up testing env'
+    Database(database=TEST_DATABASE).initialize(drop_tables=True)
+
+
+def teardown_package():
+    print 'Tearing Down'
+    kill_connections = "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{}'".format(TEST_DATABASE)
+    call(['psql', '-U', 'postgres', '-d', 'postgres', '-c', kill_connections])
+
+    call(['psql', '-U', 'postgres', '-d', 'postgres', '-c',
+          'DROP DATABASE "{}";'.format(TEST_DATABASE)])
---
+++ simpleml/tests/test_meta.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MetaClassTests(unittest.TestCase):
+    def test_abstract_method_error(self):
+        pass
+
+    def test_register_on_import(self):
+        pass
+
+    def test_duplicate_class_error(self):
+        pass
