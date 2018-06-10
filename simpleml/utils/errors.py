1f5b7f596190c9283a4ad4ecbce689041cacc243
Elisha Yadgaran <ElishaY@alum.mit.edu>
2018-06-10 17:04:45 -0400

    custom errors

---
+++ simpleml/utils/errors.py
@@ -0,0 +1,37 @@
+'''
+Error classes
+'''
+
+__author__ = 'Elisha Yadgaran'
+
+
+class SimpleMLError(StandardError):
+    def __str__(self):
+        return self.message
+
+class DatasetError(SimpleMLError):
+    def __init__(self, *args, **kwargs):
+        super(DatasetError, self).__init__(*args, **kwargs)
+        custom_prefix = 'SimpleML Dataset Error: '
+        self.message = custom_prefix + self.message
+
+
+class PipelineError(SimpleMLError):
+    def __init__(self, *args, **kwargs):
+        super(PipelineError, self).__init__(*args, **kwargs)
+        custom_prefix = 'SimpleML Pipeline Error: '
+        self.message = custom_prefix + self.message
+
+
+class ModelError(SimpleMLError):
+    def __init__(self, *args, **kwargs):
+        super(ModelError, self).__init__(*args, **kwargs)
+        custom_prefix = 'SimpleML Model Error: '
+        self.message = custom_prefix + self.message
+
+
+class MetricError(SimpleMLError):
+    def __init__(self, *args, **kwargs):
+        super(MetricError, self).__init__(*args, **kwargs)
+        custom_prefix = 'SimpleML Metric Error: '
+        self.message = custom_prefix + self.message
