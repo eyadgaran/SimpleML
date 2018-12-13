ce364f424cb843a54ce3197b33a0e885df541360
Elisha Yadgaran <ElishaY@alum.mit.edu>
2018-12-12 20:25:17 -0500

    fix failing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-from _version import __version__
+from simpleml import __version__
 
 # -- Project information -----------------------------------------------------
 
