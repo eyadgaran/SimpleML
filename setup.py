393dd5cf4a7fafe1dcbcc324e91e6a74d71f0504
Elisha Yadgaran <ElishaY@alum.mit.edu>
2020-03-15 16:28:26 -0700

    pandas min python compatibility

--- setup.py
+++ setup.py
@@ -15,6 +15,11 @@ if sys.version_info < (3, 5):  # Python < 3.5
         'scipy<1.3.0',  # Scikit-learn dependency
         'pandas<0.25.0',
     ]
+elif sys.version_info <= (3, 6, 1):  # Python 3.5/3.6
+    version_based_dependencies = [
+        'scikit-learn',
+        'pandas<1.0.0',
+    ]
 else:
     version_based_dependencies = [
         'scikit-learn',
