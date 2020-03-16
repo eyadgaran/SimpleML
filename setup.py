4f09409702b90693c48d2feee3354400784c7d8f
Elisha Yadgaran <ElishaY@alum.mit.edu>
2020-03-15 17:19:17 -0700

    3.8 testing

--- setup.py
+++ setup.py
@@ -75,5 +75,5 @@ setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,>=3.5',
+    python_requires='!=2.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,>=3.5',
 )
