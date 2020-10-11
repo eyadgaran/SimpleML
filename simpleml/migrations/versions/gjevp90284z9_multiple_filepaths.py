310b00cfbda2d794c55add1b4c77f0e40f5f7dc7
Elisha Yadgaran <eyadgaran@users.noreply.github.com>
2020-10-10 20:29:27 -0700

    Migration updates (#23)
    * postgres specifics for downgrading
    
    * migration docstring for not updating self.state.save_methods
    
    * reversal. upgrade and downgrade metadata state as well
    
    * pass session around explicitly

--- simpleml/migrations/versions/gjevp90284z9_multiple_filepaths.py
+++ simpleml/migrations/versions/gjevp90284z9_multiple_filepaths.py
@@ -33,6 +33,7 @@ class UpgradeTableModel(BaseSQLAlchemy):
     __abstract__ = True
     metadata = MetaData()
     id = Column(GUID, primary_key=True)
+    metadata_ = Column('metadata', JSON, default={})
     filepaths = Column(JSON)
 
 
@@ -78,7 +79,7 @@ def upgrade():
             (DatasetModel, PipelineModel, ModelsModel, MetricsModel),
             ('dataset', 'pipeline', 'model', 'metric')
         ):
-            upgrade_data(table, artifact)
+            upgrade_data(session, table, artifact)
         session.commit()
     except Exception:
         session.rollback()
@@ -87,31 +88,52 @@ def upgrade():
         session.close()
 
 
-def upgrade_data(table, artifact):
+def upgrade_data(session, table, artifact):
     # get all nonnull records
-    records = table.where(filepaths__ne={}).all()
-    LOGGER.info(f'Modifying filepath data for {len(records)} records')
+    records = table.all()
+    LOGGER.info(f'Modifying data for {len(records)} records')
     for record in records:
-        # Nest under artifact
-        filepath = {artifact: record.filepaths}
-        # Format the filepath data according to the save pattern
-        for save_pattern, filepath_data in filepath[artifact].items():
-            if save_pattern == 'database_table':
-                # Format changed from [(schema, table)] to {schema: schema, table: table}
-                filepath[artifact][save_pattern] = {'schema': filepath_data[0][0], 'table': filepath_data[0][1]}
-            elif save_pattern == 'database_pickled':
-                # Format changed from [record_id] to record_id
-                filepath[artifact][save_pattern] = filepath_data[0]
-            elif save_pattern in ['disk_pickled', 'disk_hdf5', 'disk_keras_hdf5',
-                                  'onedrive_pickled', 'onedrive_hdf5', 'onedrive_keras_hdf5',
-                                  'cloud_pickled', 'cloud_hdf5', 'cloud_keras_hdf5']:
-                # Format changed from [filename] to filename
-                filepath[artifact][save_pattern] = filepath_data[0]
-            # Else leave as is, not a natively supported save pattern
-        record.filepaths = filepath
+        # Modify save method in metadata
+        if 'save_method' in record.metadata_.get('state', {}):
+            upgrade_metadata(record, artifact)
+
+        # Modify filepaths
+        if record.filepaths:
+            upgrade_filepaths(record, artifact)
 
     if records:
-        table._session.add_all(records)
+        session.add_all(records)
+
+
+def upgrade_metadata(record, artifact):
+    '''
+    Upgrade metadata field for each record
+    '''
+    pattern = record.metadata_['state'].pop('save_method')
+    record.metadata_['state']['save_patterns'] = {artifact: [pattern]}
+
+
+def upgrade_filepaths(record, artifact):
+    '''
+    Upgrade filepaths field for each record
+    '''
+    # Nest under artifact
+    filepath = {artifact: record.filepaths}
+    # Format the filepath data according to the save pattern
+    for save_pattern, filepath_data in filepath[artifact].items():
+        if save_pattern == 'database_table':
+            # Format changed from [(schema, table)] to {schema: schema, table: table}
+            filepath[artifact][save_pattern] = {'schema': filepath_data[0][0], 'table': filepath_data[0][1]}
+        elif save_pattern == 'database_pickled':
+            # Format changed from [record_id] to record_id
+            filepath[artifact][save_pattern] = filepath_data[0]
+        elif save_pattern in ['disk_pickled', 'disk_hdf5', 'disk_keras_hdf5',
+                              'onedrive_pickled', 'onedrive_hdf5', 'onedrive_keras_hdf5',
+                              'cloud_pickled', 'cloud_hdf5', 'cloud_keras_hdf5']:
+            # Format changed from [filename] to filename
+            filepath[artifact][save_pattern] = filepath_data[0]
+        # Else leave as is, not a natively supported save pattern
+    record.filepaths = filepath
 
 
 def downgrade():
@@ -124,7 +146,7 @@ def downgrade():
             (DatasetModel, PipelineModel, ModelsModel, MetricsModel),
             ('dataset', 'pipeline', 'model', 'metric')
         ):
-            downgrade_data(table, artifact)
+            downgrade_data(session, table, artifact)
         session.commit()
     except Exception:
         session.rollback()
@@ -133,27 +155,49 @@ def downgrade():
         session.close()
 
 
-def downgrade_data(table, artifact):
+def downgrade_data(session, table, artifact):
     # get all nonnull records
-    records = table.where(filepaths__ne={}).all()
-    LOGGER.info(f'Modifying filepath data for {len(records)} records')
+    records = table.all()
+    LOGGER.info(f'Modifying data for {len(records)} records')
     for record in records:
-        # unnest
-        save_pattern, filepath = list(record.filepaths[artifact].items())[0]
-        # Format the filepath data according to the save pattern
-        if save_pattern == 'database_table':
-            # Format changed from [(schema, table)] to {schema: schema, table: table}
-            filepath = [(filepath['schema'], filepath['table'])]
-        elif save_pattern == 'database_pickled':
-            # Format changed from [record_id] to record_id
-            filepath = [filepath]
-        elif save_pattern in ['disk_pickled', 'disk_hdf5', 'disk_keras_hdf5',
-                              'onedrive_pickled', 'onedrive_hdf5', 'onedrive_keras_hdf5',
-                              'cloud_pickled', 'cloud_hdf5', 'cloud_keras_hdf5']:
-            # Format changed from [filename] to filename
-            filepath = [filepath]
-        # Else leave as is, not a natively supported save pattern
-        record.filepaths = {save_pattern: filepath}
-
+        # Modify save method in metadata
+        if 'save_patterns' in record.metadata_.get('state', {}):
+            downgrade_metadata(record, artifact)
+        # Modify filepaths
+        if record.filepaths:
+            downgrade_filepaths(record, artifact)
     if records:
-        table._session.add_all(records)
+        session.add_all(records)
+
+
+def downgrade_metadata(record, artifact):
+    '''
+    Downgrade metadata field for each record
+    '''
+    save_patterns = record.metadata_['state'].pop('save_patterns')
+    if len(save_patterns) > 1 or len(save_patterns[artifact]) > 1:
+        # There are extra patterns or artifacts that arent backwards compatible
+        LOGGER.error('Found non backwards compatible artifact data, this will be lost!')
+    record.metadata_['state']['save_method'] = save_patterns[artifact][0]
+
+
+def downgrade_filepaths(record, artifact):
+    '''
+    Downgrade filepaths field for each record
+    '''
+    # unnest
+    save_pattern, filepath = list(record.filepaths[artifact].items())[0]
+    # Format the filepath data according to the save pattern
+    if save_pattern == 'database_table':
+        # Format changed from [(schema, table)] to {schema: schema, table: table}
+        filepath = [(filepath['schema'], filepath['table'])]
+    elif save_pattern == 'database_pickled':
+        # Format changed from [record_id] to record_id
+        filepath = [filepath]
+    elif save_pattern in ['disk_pickled', 'disk_hdf5', 'disk_keras_hdf5',
+                          'onedrive_pickled', 'onedrive_hdf5', 'onedrive_keras_hdf5',
+                          'cloud_pickled', 'cloud_hdf5', 'cloud_keras_hdf5']:
+        # Format changed from [filename] to filename
+        filepath = [filepath]
+    # Else leave as is, not a natively supported save pattern
+    record.filepaths = {save_pattern: filepath}
