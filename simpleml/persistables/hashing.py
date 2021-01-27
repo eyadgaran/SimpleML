6cbbb9ac9def86d7b3c8082e062e4f01938f95d8
Elisha Yadgaran <eyadgaran@users.noreply.github.com>
2021-01-26 23:30:32 -0800

    Minor fixes (#34)
    * bug in dataset creator
    
    * make hashing a classmethod
    
    * release notes

--- simpleml/persistables/hashing.py
+++ simpleml/persistables/hashing.py
@@ -15,7 +15,8 @@ class CustomHasherMixin(object):
     '''
     Mixin class to hash any object
     '''
-    def custom_hasher(self, object_to_hash, custom_class_proxy=type(object.__dict__)):
+    @classmethod
+    def custom_hasher(cls, object_to_hash, custom_class_proxy=type(object.__dict__)):
         """
         Adapted from: https://stackoverflow.com/questions/5884066/hashing-a-dictionary
         Makes a hash from a dictionary, list, tuple or set to any level, that
@@ -42,21 +43,21 @@ class CustomHasherMixin(object):
             object_to_hash = o2
 
         if isinstance(object_to_hash, (set, tuple, list)):
-            return hash(tuple([self.custom_hasher(e) for e in object_to_hash]))
+            return hash(tuple([cls.custom_hasher(e) for e in object_to_hash]))
 
         elif isinstance(object_to_hash, np.ndarray):
-            return self.custom_hasher(object_to_hash.tostring())
+            return cls.custom_hasher(object_to_hash.tostring())
 
         elif isinstance(object_to_hash, pd.DataFrame):
             # Pandas is unable to hash numpy arrays so prehash those
             return hash_pandas_object(object_to_hash.applymap(
-                lambda element: self.custom_hasher(element) if isinstance(element, np.ndarray) else element),
+                lambda element: cls.custom_hasher(element) if isinstance(element, np.ndarray) else element),
                 index=False).sum()
 
         elif isinstance(object_to_hash, pd.Series):
             # Pandas is unable to hash numpy arrays so prehash those
             return hash_pandas_object(object_to_hash.apply(
-                lambda element: self.custom_hasher(element) if isinstance(element, np.ndarray) else element),
+                lambda element: cls.custom_hasher(element) if isinstance(element, np.ndarray) else element),
                 index=False).sum()
 
         elif object_to_hash is None:
@@ -65,19 +66,19 @@ class CustomHasherMixin(object):
 
         elif isinstance(object_to_hash, dict):
             return hash(tuple(
-                sorted([self.custom_hasher(item) for item in object_to_hash.items()])
+                sorted([cls.custom_hasher(item) for item in object_to_hash.items()])
             ))
 
         elif isinstance(object_to_hash, type(lambda: 0)):
             # Functions dont hash consistently because of the halting problem
             # https://stackoverflow.com/questions/33998594/hash-for-lambda-function-in-python
             # Attempt to use the source code string
-            return self.custom_hasher(inspect.getsource(object_to_hash))
+            return cls.custom_hasher(inspect.getsource(object_to_hash))
 
         elif isinstance(object_to_hash, type):
             # Have to keep this at the end of the try list; np.ndarray,
             # pd.DataFrame/Series, and function are also of <type 'type'>
-            return self.custom_hasher(repr(object_to_hash))
+            return cls.custom_hasher(repr(object_to_hash))
             # return self.custom_hasher(inspect.getsource(object_to_hash))
 
         else:
@@ -123,6 +124,7 @@ class _ConsistentSet(object):
     """ Class used to ensure the hash of Sets is preserved
         whatever the order of its items.
     """
+
     def __init__(self, set_sequence):
         # Forces order of elements in set to ensure consistent hash.
         try:
