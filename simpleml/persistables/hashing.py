572b7aac4d2f6d61a8ce7a1873e12f731fac65ae
Elisha Yadgaran <ElishaY@alum.mit.edu>
2021-05-10 00:04:42 -0700

    logging for traceability

--- simpleml/persistables/hashing.py
+++ simpleml/persistables/hashing.py
@@ -8,11 +8,15 @@ __author__ = 'Elisha Yadgaran'
 import pandas as pd
 import numpy as np
 import inspect
+import logging
 
 from pandas.util import hash_pandas_object
 from typing import Any, Type
 
 from simpleml._external.joblib import hash as deterministic_hash
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class CustomHasherMixin(object):
@@ -41,6 +45,8 @@ class CustomHasherMixin(object):
         seed. Need to set the global PYTHONHASHSEED=0 or use a different hash
         function
         """
+        LOGGER.debug(f'Hashing input: {object_to_hash}')
+
         if type(object_to_hash) == custom_class_proxy:
             o2 = {}
             for k, v in object_to_hash.items():
@@ -48,30 +54,32 @@ class CustomHasherMixin(object):
                     o2[k] = v
             object_to_hash = o2
 
+        LOGGER.debug(f'hash type: {type(object_to_hash)}')
+
         if isinstance(object_to_hash, (set, tuple, list)):
-            return deterministic_hash(tuple([cls.custom_hasher(e) for e in object_to_hash]))
+            hash_output = deterministic_hash(tuple([cls.custom_hasher(e) for e in object_to_hash]))
 
         elif isinstance(object_to_hash, np.ndarray):
-            return cls.custom_hasher(object_to_hash.tostring())
+            hash_output = cls.custom_hasher(object_to_hash.tostring())
 
         elif isinstance(object_to_hash, pd.DataFrame):
             # Pandas is unable to hash numpy arrays so prehash those
-            return hash_pandas_object(object_to_hash.applymap(
+            hash_output = hash_pandas_object(object_to_hash.applymap(
                 lambda element: cls.custom_hasher(element) if isinstance(element, np.ndarray) else element),
                 index=False).sum()
 
         elif isinstance(object_to_hash, pd.Series):
             # Pandas is unable to hash numpy arrays so prehash those
-            return hash_pandas_object(object_to_hash.apply(
+            hash_output = hash_pandas_object(object_to_hash.apply(
                 lambda element: cls.custom_hasher(element) if isinstance(element, np.ndarray) else element),
                 index=False).sum()
 
         elif object_to_hash is None:
             # hash of None is unstable between systems
-            return -12345678987654321
+            hash_output = -12345678987654321
 
         elif isinstance(object_to_hash, dict):
-            return deterministic_hash(tuple(
+            hash_output = deterministic_hash(tuple(
                 sorted([cls.custom_hasher(item) for item in object_to_hash.items()])
             ))
 
@@ -79,13 +87,16 @@ class CustomHasherMixin(object):
             # Functions dont hash consistently because of the halting problem
             # https://stackoverflow.com/questions/33998594/hash-for-lambda-function-in-python
             # Attempt to use the source code string
-            return cls.custom_hasher(inspect.getsource(object_to_hash))
+            hash_output = cls.custom_hasher(inspect.getsource(object_to_hash))
 
         elif isinstance(object_to_hash, type):
             # Have to keep this at the end of the try list; np.ndarray,
             # pd.DataFrame/Series, and function are also of <type 'type'>
-            return cls.custom_hasher(repr(object_to_hash))
+            hash_output = cls.custom_hasher(repr(object_to_hash))
             # return self.custom_hasher(inspect.getsource(object_to_hash))
 
         else:
-            return deterministic_hash(object_to_hash)
+            hash_output = deterministic_hash(object_to_hash)
+
+        LOGGER.debug(f'Hashing output: {hash_output}')
+        return hash_output
