4cd3dc8a69f4646f13fc86afb4cf9d5edba8e65e
Elisha Yadgaran <elishay@alum.mit.edu>
2021-12-31 12:14:25 -0500

    composed save patterns

--- simpleml/save_patterns/__init__.py
+++ simpleml/save_patterns/__init__.py
@@ -1,9 +1,14 @@
 '''
-Package for the different save patterns available
+Package for artifact persistence. Bindings are automatically included for SimpleML
+persistables, but patterns can be used for any objects or frameworks.
+
 Patterns are loaded into global registry on import and more can be added
 externally by decorating
 
-Nomenclature -> Save Location : Save Format
+Patterns can be named anything since they are only mappings in the registry.
+Convention is -> Location : Serializer : Format(s)
+
+
 
 - Database Storage
     - database_table: Dataframe saving (as tables in dedicated schema)
@@ -35,11 +40,52 @@ Nomenclature -> Save Location : Save Format
 __author__ = 'Elisha Yadgaran'
 
 
-# Auto import all submodules to ensure registration on library import
-from .base import SavePatternMixin, BaseSavePattern
-from .decorators import SavePatternDecorators, register_save_pattern, deregister_save_pattern
+import logging
+from os.path import join
+from typing import Any, Optional
 
-from . import database
-from . import libcloud
-from . import local
-from . import onedrive
+from simpleml.utils.configuration import PICKLE_DIRECTORY
+
+# Auto import all submodules to ensure registration on library import
+from .base import BaseSavePattern, BaseSerializer
+from .decorators import (SavePatternDecorators, deregister_save_pattern,
+                         register_save_pattern)
+from .locations.disk import (FilestoreCopyFileLocation,
+                             FilestoreCopyFilesLocation,
+                             FilestoreCopyFolderLocation,
+                             FilestorePassthroughLocation)
+from .locations.libcloud import (LibcloudCopyFileLocation,
+                                 LibcloudCopyFilesLocation,
+                                 LibcloudCopyFolderLocation)
+from .serializers.cloudpickle import CloudpickleFileSerializer
+from .serializers.dask import DaskCSVSerializer, DaskParquetSerializer
+
+LOGGER = logging.getLogger(__name__)
+
+
+'''
+(Cloud)Pickle Save Patterns
+'''
+
+
+
+'''
+Dask Save Patterns
+'''
+
+
+'''
+Pandas Save Patterns
+'''
+
+'''
+Keras Save Patterns
+'''
+
+'''
+Hickle Save Patterns
+'''
+
+'''
+Database Save Patterns
+'''
