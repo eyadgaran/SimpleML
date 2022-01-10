9dc17583bd1f352409cd84398145e687eea66ed8
Elisha Yadgaran <elishay@alum.mit.edu>
2022-01-09 19:09:50 -0800

    testing utils - handle index naming

--- simpleml/tests/utils.py
+++ simpleml/tests/utils.py
@@ -12,11 +12,13 @@ import pandas as pd
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal, assert_series_equal
 from simpleml.datasets.dataset_splits import Split
+from simpleml.imports import ddDataFrame
 from simpleml.pipelines.projected_splits import ProjectedDatasetSplit
 
 # Test Data Paths
 DATA_PATH = join(dirname(abspath(__file__)), 'data')
 ARTIFACTS_PATH = join(DATA_PATH, 'artifacts')
+DATABASES_PATH = join(DATA_PATH, 'databases')
 MOCK_PATH = join(DATA_PATH, 'mock')
 
 
@@ -24,8 +26,21 @@ def assert_data_container_equal(a: Any, b: Any):
     '''
     Dynamic wrapper for custom equality functions
     '''
+    def _simpleml_frame_equal(a, b):
+        '''
+        SimpleML overwrites index for certain patterns to maintain an absolute
+        reference on lossy patterns (csv, json, etc)
+        Evaluate separately, ignoring index name
+        '''
+        assert_frame_equal(a, b, check_names=False)
+        assert(a.columns.tolist() == b.columns.tolist())
+
     if isinstance(a, pd.DataFrame) or isinstance(b, pd.DataFrame):
-        assert_frame_equal(a, b)
+        _simpleml_frame_equal(a, b)
+    elif isinstance(a, ddDataFrame) or isinstance(b, ddDataFrame):
+        a_df = a.compute()
+        b_df = b.compute()
+        _simpleml_frame_equal(a_df, b_df)
     elif isinstance(a, pd.Series) or isinstance(b, pd.Series):
         assert_series_equal(a, b)
     elif isinstance(a, np.ndarray) or isinstance(b, np.ndarray):
