4c3d01856d44f7e660566d52bdfa521053218376
Elisha Yadgaran <elishay@alum.mit.edu>
2022-01-25 23:26:44 -0800

    linting

--- simpleml/executors/multiprocessing/__init__.py
+++ simpleml/executors/multiprocessing/__init__.py
@@ -2,7 +2,7 @@
 Executor support for multiprocessing backend
 
 Multiprocessing in this context refers to a data specific operation.
-Applying a function in parallel across the available processes. 
+Applying a function in parallel across the available processes.
 '''
 
 __author__ = 'Elisha Yadgaran'
