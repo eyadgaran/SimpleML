c6fe621f69a7ae9a91c86b9068b034b3a112efce
Elisha Yadgaran <elishay@alum.mit.edu>
2022-02-13 13:50:13 -0800

    integration tests

--- simpleml/pipelines/sklearn/__init__.py
+++ simpleml/pipelines/sklearn/__init__.py
@@ -5,6 +5,6 @@ Pipeline Library support for Scikit-Learn
 __author__ = 'Elisha Yadgaran'
 
 
-from .base import SklearnExternalPipeline
+from .base import SklearnPipeline
 from .split_pipelines import (ExplicitSplitSklearnPipeline,
                               RandomSplitSklearnPipeline)
