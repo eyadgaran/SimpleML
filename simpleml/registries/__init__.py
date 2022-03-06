3757c04784ec5039d2aceec4f31003441150cf17
Elisha Yadgaran <elishay@alum.mit.edu>
2022-03-05 16:08:28 -0800

    orm registry

--- simpleml/registries/__init__.py
+++ simpleml/registries/__init__.py
@@ -6,19 +6,17 @@ __author__ = "Elisha Yadgaran"
 
 
 from simpleml.registries.keras_registry import KERAS_REGISTRY, KerasRegistry
+from simpleml.registries.persistable_registry import (DATASET_REGISTRY,
+                                                      METRIC_REGISTRY,
+                                                      MODEL_REGISTRY,
+                                                      PIPELINE_REGISTRY,
+                                                      SIMPLEML_REGISTRY,
+                                                      DatasetRegistry,
+                                                      MetricRegistry,
+                                                      ModelRegistry,
+                                                      PersistableRegistry,
+                                                      PipelineRegistry)
 from simpleml.registries.registry import NamedRegistry, Registry
-from simpleml.registries.sqlalchemy_registry import (
-    DATASET_REGISTRY,
-    METRIC_REGISTRY,
-    MODEL_REGISTRY,
-    PIPELINE_REGISTRY,
-    SIMPLEML_REGISTRY,
-    DatasetRegistry,
-    MetaRegistry,
-    MetricRegistry,
-    ModelRegistry,
-    PipelineRegistry,
-)
 
 # Registry for save patterns
 SAVE_METHOD_REGISTRY = NamedRegistry()
@@ -26,3 +24,6 @@ LOAD_METHOD_REGISTRY = NamedRegistry()
 
 # Registry for dynamic system filepaths
 FILEPATH_REGISTRY = NamedRegistry()
+
+# Registry for ORM Classes
+ORM_REGISTRY = NamedRegistry()
