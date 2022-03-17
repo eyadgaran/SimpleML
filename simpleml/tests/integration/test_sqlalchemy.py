b96d4fde792014bbc05cbe05fa872c48e568f134
Elisha Yadgaran <elishay@alum.mit.edu>
2022-03-16 22:44:45 -0700

    create table only once

--- simpleml/tests/integration/test_sqlalchemy.py
+++ simpleml/tests/integration/test_sqlalchemy.py
@@ -6,41 +6,47 @@ __author__ = "Elisha Yadgaran"
 
 
 import unittest
+import uuid
 
-from simpleml.orm.persistable import ORMPersistable
+from simpleml.orm.metadata import SimplemlCoreSqlalchemy
+from simpleml.orm.sqlalchemy_types import GUID, MutableJSON
+from sqlalchemy import Column
 
 
 class MutableJSONTests(unittest.TestCase):
-    '''Default sqlalchemy behavior treats JSON data as immutable'''
-    class JSONTestClass(ORMPersistable):
-        __tablename__ = 'json_tests'
+    """Default sqlalchemy behavior treats JSON data as immutable"""
+
+    class JSONTestClass(SimplemlCoreSqlalchemy):
+        __tablename__ = "json_tests"
+        id = Column(GUID, primary_key=True, nullable=False, default=uuid.uuid4)
+        json_col = Column(MutableJSON)
 
     @classmethod
     def setUpClass(cls):
-        cls.JSONTestClass.__table__.create()
+        cls.JSONTestClass.__table__.create(checkfirst=True)
 
     def test_modifying_json_field(self):
         """
         Top level JSON change
-        '''
-        persistable = self.JSONTestClass(name='top_level_json_modification_test')
-        persistable.metadata_ = {}
+        """
+        persistable = self.JSONTestClass()
+        persistable.json_col = {}
         persistable.save()
 
-        persistable.metadata_["new_key"] = "blah"
+        persistable.json_col["new_key"] = "blah"
         self.assertIn(persistable, persistable._session.dirty)
         persistable._session.refresh(persistable)
 
     def test_modifying_nested_json_field(self):
         """
         Nested JSON change
-        '''
-        persistable = self.JSONTestClass(name='nested_json_modification_test')
-        persistable.metadata_ = {}
-        persistable.metadata_['new_key'] = {}
+        """
+        persistable = self.JSONTestClass()
+        persistable.json_col = {}
+        persistable.json_col["new_key"] = {}
         persistable.save()
 
-        persistable.metadata_["new_key"]["sub_key"] = "blah"
+        persistable.json_col["new_key"]["sub_key"] = "blah"
         self.assertIn(persistable, persistable._session.dirty)
         persistable._session.refresh(persistable)
 
